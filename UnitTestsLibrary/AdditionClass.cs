467c61243fdee25c1dae80d4f13a12ff0ce87cf0
Theodore Pollock <pollockt@tulsapretech.com>
2021-06-30 23:04:07 -0500

    Add project files.

---
+++ UnitTestsLibrary/AdditionClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestsLibrary
+{
+    public class AdditionClass
+    {
+        public int FirstNum { get; set; }
+        public int SecondNum { get; set; }
+        public AdditionClass()
+        {
+
+        }
+
+        public AdditionClass(int a, int b)
+        {
+            FirstNum = a;
+            SecondNum = b;
+        }
+        public int AdditionCall(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ UnitTestsLibrary/DivisionClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestsLibrary
+{
+    public class DivisionClass
+    {
+        public int FirstNum { get; set; }
+        public int SecondNum { get; set; }
+        public DivisionClass()
+        {
+
+        }
+
+        public DivisionClass(int a, int b)
+        {
+            FirstNum = a;
+            SecondNum = b;
+        }
+        public int DivisionCall(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
